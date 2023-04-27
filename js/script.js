0e0eab772467f5baec95a99dd125101fa4a8b481
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-04-27 17:26:17 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function generates a random number between 1 and 6
+ */
+const randomPositiveNumber = Math.floor(Math.random() * 6) + 1 // returns a random integer from 1 to 6 into variable "randomNumber"
+
+/**
+ *  * This function generates a random number between 1 and 6
+ */
+const randomNegativeNumber = Math.floor(Math.random() * 6) + 1 // returns a random integer from 1 to 6 into variable "randomNumber"
+
+/**
+ * This function displays the radio value.
+ */
+function myButtonClicked() {
+  alwaysOnButtonChecked = document.getElementById('on-check').checked
+
+  if (alwaysOnButtonChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+    + randomPositiveNumber +
+  } else {
+
+    document.getElementById("radio-button-value").innerHTML =
+    + randomNegativeNumber +
+  }
+}
